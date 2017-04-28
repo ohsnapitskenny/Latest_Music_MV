88b95d827f253c1a69b9a601ed98f95e06c01ab1
aznkenny <hk_lam@live.com>
2017-04-28 16:21:43 +0200

    Initial Commit

---
+++ Latest_Music_MV/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Latest_Music_MV
+//
+//  Created by Kenny Lam on 28/04/2017.
+//  Copyright © 2017 Kenny Lam. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
